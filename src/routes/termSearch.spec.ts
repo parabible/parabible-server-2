aaa44c8e675765739782e92de6b2a7a3a54e323b
James Cuénod <jcuenod@users.noreply.github.com>
2022-07-27 22:23:27 -0500

    Don't use steps in tests unless it's stepped

--- src/routes/termSearch.spec.ts
+++ src/routes/termSearch.spec.ts
@@ -1,51 +1,40 @@
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts"
 import { get as getTermSearch } from "./termSearch.ts"
 
-Deno.test("Term Search Route", async (t) => {
-	await t.step({
-		name: "should return the right number of words from BHSA",
-		fn: async () => {
-			const { count } = await getTermSearch({
-				searchTerms: [{ invert: false, data: { lexeme: "אמר" } }],
-				treeNodeType: "clause",
-				modules: "ETCBC+BHSA,LXX,Nestle1904",
-				corpusFilter: "",
-				versificationSchema: "kjv",
-			})
-			assertEquals(count, 5373)
-		}
+Deno.test("should return results from BHSA", async () => {
+	const { count } = await getTermSearch({
+		searchTerms: [{ invert: false, data: { lexeme: "אמר" } }],
+		treeNodeType: "clause",
+		modules: "ETCBC+BHSA,LXX,Nestle1904",
+		corpusFilter: "",
+		versificationSchema: "kjv",
 	})
+	assertEquals(count, 5373)
+})
 
-	await t.step({
-		name: "should normalize unicode characters",
-		fn: async () => {
-			const { count } = await getTermSearch({
-				searchTerms: [{ invert: false, data: { lexeme: "נפשׁ" } }],
-				treeNodeType: "clause",
-				modules: "ETCBC+BHSA,LXX,Nestle1904",
-				corpusFilter: "",
-				versificationSchema: "kjv",
-			})
-			assertEquals(count, 3)
-		}
+Deno.test("should normalize unicode characters", async () => {
+	const { count } = await getTermSearch({
+		searchTerms: [{ invert: false, data: { lexeme: "נפשׁ" } }],
+		treeNodeType: "clause",
+		modules: "ETCBC+BHSA,LXX,Nestle1904",
+		corpusFilter: "",
+		versificationSchema: "kjv",
 	})
+	assertEquals(count, 3)
+})
 
-	await t.step({
-		name: "should work with inverted rules",
-		fn: async () => {
-			const { count } = await getTermSearch({
-				searchTerms: [
-					{ invert: false, data: { lexeme: "נפשׁ" } },
-					{ invert: true, data: { lexeme: "נוח" } },
-				],
-				treeNodeType: "parallel",
-				modules: "ETCBC+BHSA,LXX,Nestle1904",
-				corpusFilter: "",
-				versificationSchema: "kjv",
-			})
-			assertEquals(count, 2)
-		}
+Deno.test("should work with inverted rules", async () => {
+	const { count } = await getTermSearch({
+		searchTerms: [
+			{ invert: false, data: { lexeme: "נפשׁ" } },
+			{ invert: true, data: { lexeme: "נוח" } },
+		],
+		treeNodeType: "parallel",
+		modules: "ETCBC+BHSA,LXX,Nestle1904",
+		corpusFilter: "",
+		versificationSchema: "kjv",
 	})
+	assertEquals(count, 2)
 })
 
 
