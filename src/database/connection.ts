5157d5a1a47a51871da240adb7064c53abc18450
James Cuénod <4253884+jcuenod@users.noreply.github.com>
2023-05-15 21:36:01 -0500

    Deno fmt

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,26 +1,26 @@
-import type { ClickhouseResponse } from "../../types.d.ts"
-const USERNAME = Deno.env.get("CLICKHOUSE_USER") || "admin"
-const PASSWORD = Deno.env.get("CLICKHOUSE_PASSWORD") || "toor"
-const SERVER_URL = Deno.env.get("CLICKHOUSE_URL") || "http://localhost:8123"
+import type { ClickhouseResponse } from "../../types.d.ts";
+const USERNAME = Deno.env.get("CLICKHOUSE_USER") || "admin";
+const PASSWORD = Deno.env.get("CLICKHOUSE_PASSWORD") || "toor";
+const SERVER_URL = Deno.env.get("CLICKHOUSE_URL") || "http://localhost:8123";
 
-const query = <T>(query: string) => new Promise<ClickhouseResponse<T>>((resolve, reject) => {
-	const queryString =
-		SERVER_URL +
-		"/?query=" +
-		encodeURIComponent(query + " FORMAT JSON")
+const query = <T>(query: string) =>
+  new Promise<ClickhouseResponse<T>>((resolve, reject) => {
+    const queryString = SERVER_URL +
+      "/?query=" +
+      encodeURIComponent(query + " FORMAT JSON");
 
-	fetch(queryString, {
-		headers: {
-			"X-ClickHouse-User": USERNAME,
-			"X-ClickHouse-Key": PASSWORD,
-		}
-	}).then(r => r.json()).then((r: ClickhouseResponse<T>) => {
-		resolve(r)
-	}).catch(e => {
-		console.error("DATABASE ERROR")
-		console.error(query)
-		console.error(e)
-		reject(e)
-	})
-})
-export { query }
+    fetch(queryString, {
+      headers: {
+        "X-ClickHouse-User": USERNAME,
+        "X-ClickHouse-Key": PASSWORD,
+      },
+    }).then((r) => r.json()).then((r: ClickhouseResponse<T>) => {
+      resolve(r);
+    }).catch((e) => {
+      console.error("DATABASE ERROR");
+      console.error(query);
+      console.error(e);
+      reject(e);
+    });
+  });
+export { query };
