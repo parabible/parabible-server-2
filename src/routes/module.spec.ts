23b2b51ab4cf70f89b278c08e07992f6a165abb7
James Cuénod <4253884+jcuenod@users.noreply.github.com>
2023-05-15 21:29:31 -0500

    Deno fmt + extra assertions

--- src/routes/module.spec.ts
+++ src/routes/module.spec.ts
@@ -1,13 +1,15 @@
-import { assertEquals } from "https://deno.land/std/testing/asserts.ts"
-import { get as getModule } from "./module.ts"
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import { get as getModule } from "./module.ts";
 
 Deno.test("Module Route", async (t) => {
-	await t.step({
-		name: "should return a list of modules",
-		fn: async () => {
-			const { data: modules } = await getModule()
-			assertEquals(modules.length > 0, true)
-			assertEquals(!!modules.find(m => m.abbreviation === "ETCBC BHSA"), true)
-		}
-	})
-})
+  await t.step({
+    name: "should return a list of modules that includes BHSA, NET, and NA1904",
+    fn: async () => {
+      const { data: modules } = await getModule();
+      assertEquals(modules.length > 0, true);
+      assertEquals(!!modules.find((m) => m.abbreviation === "BHSA"), true);
+      assertEquals(!!modules.find((m) => m.abbreviation === "NA1904"), true);
+      assertEquals(!!modules.find((m) => m.abbreviation === "NET"), true);
+    },
+  });
+});
